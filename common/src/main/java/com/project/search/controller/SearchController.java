92dd489072b252f3c3d52bd23fdb0c939b21e1da
jkkim <kook3238@gmail.com>
2023-03-19 02:21:12 +0900

    dd

---
+++ common/src/main/java/com/project/search/controller/SearchController.java
@@ -0,0 +1,19 @@
+package com.project.search.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SearchController {
+    @Autowired
+    //private SearchService searchService;
+
+    @GetMapping("/popularSearch")
+    public String test(@RequestParam int date) {
+        return "";
+    }
+}
